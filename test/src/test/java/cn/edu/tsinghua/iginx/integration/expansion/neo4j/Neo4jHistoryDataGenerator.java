b38dff52f2b38d1e9256bd9d8abfba99fb978653
chaos <958273999@qq.com>
2025-04-06 11:43:09 +0800

    add neo4j database

---
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/neo4j/Neo4jHistoryDataGenerator.java
@@ -0,0 +1,168 @@
+/*
+ * IGinX - the polystore system with high performance
+ * Copyright (C) Tsinghua University
+ * TSIGinX@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package cn.edu.tsinghua.iginx.integration.expansion.neo4j;
+
+import cn.edu.tsinghua.iginx.integration.expansion.BaseHistoryDataGenerator;
+import cn.edu.tsinghua.iginx.integration.expansion.constant.Constant;
+import cn.edu.tsinghua.iginx.neo4j.tools.Neo4jClientUtils;
+import cn.edu.tsinghua.iginx.thrift.DataType;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import org.neo4j.driver.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Neo4jHistoryDataGenerator extends BaseHistoryDataGenerator {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jHistoryDataGenerator.class);
+
+  static final String LOCAL_IP = "127.0.0.1";
+
+  private static final char SEPARATOR = '.';
+  private static final String USER = "neo4j";
+  private static final String PASSWORD = "neo4jtest";
+
+  private static final String KEY_NAME = "KEYID";
+
+  public Neo4jHistoryDataGenerator() {
+    Constant.oriPort = 7687;
+    Constant.expPort = 7688;
+    Constant.readOnlyPort = 7689;
+    LOGGER.info("LOCAL_IP : {}", LOCAL_IP);
+  }
+
+  @Override
+  public void writeHistoryData(
+      int port,
+      List<String> pathList,
+      List<DataType> dataTypeList,
+      List<Long> keyList,
+      List<List<Object>> valuesList) {
+    doWriteHistoryData(port, pathList, dataTypeList, keyList, valuesList);
+  }
+
+  public void doWriteHistoryData(
+      int port,
+      List<String> pathList,
+      List<DataType> dataTypeList,
+      List keyList,
+      List<List<Object>> valuesList) {
+    try (Driver driver = createBoltDriver(port);
+        Session session = driver.session()) {
+      Map<String, List<Integer>> labelToColumnIndexes = new HashMap<>();
+      for (int i = 0; i < pathList.size(); i++) {
+        String path = pathList.get(i);
+        String labelName = path.substring(0, path.lastIndexOf(SEPARATOR));
+
+        List<Integer> columnIndexes =
+            labelToColumnIndexes.computeIfAbsent(labelName, x -> new ArrayList<>());
+        columnIndexes.add(i);
+      }
+
+      DataType idType = DataType.LONG;
+      if (keyList != null
+          && keyList.size() > 0
+          && !(keyList.get(0) instanceof Long || keyList.get(0) instanceof Integer)) {
+        idType = DataType.BINARY;
+      }
+      for (Map.Entry<String, List<Integer>> item : labelToColumnIndexes.entrySet()) {
+        String labelName = item.getKey();
+        Neo4jClientUtils.checkAndCreateUniqueConstraint(session, labelName, KEY_NAME);
+
+        List<Map<String, Object>> data = new ArrayList<>();
+        int id = 0;
+        for (int i = 0; i < valuesList.size(); i++) {
+          List<Object> values = valuesList.get(i);
+          Map<String, Object> row = new HashMap<>();
+          for (Integer index : item.getValue()) {
+            String path = pathList.get(index);
+            String propertyName = path.substring(path.lastIndexOf(SEPARATOR) + 1);
+            row.put(propertyName, values.get(index));
+          }
+          if (keyList != null && keyList.size() > i) {
+            if (idType == DataType.LONG) {
+              row.put(KEY_NAME, ((Number) keyList.get(i)).longValue());
+            } else {
+              row.put(KEY_NAME, keyList.get(i));
+            }
+          } else {
+            if (idType == DataType.LONG) {
+              row.put(KEY_NAME, id++);
+            } else {
+              row.put(KEY_NAME, String.valueOf(id++));
+            }
+          }
+
+          data.add(row);
+        }
+
+        Neo4jClientUtils.bulkInsert(session, labelName, KEY_NAME, data);
+        LOGGER.info("complete insertRows.");
+      }
+
+      LOGGER.info("write data to " + LOCAL_IP + ":{} success!", port);
+    } catch (Exception e) {
+      LOGGER.error("unexpected error: ", e);
+    }
+  }
+
+  @Override
+  public void writeHistoryData(
+      int port, List<String> pathList, List<DataType> dataTypeList, List<List<Object>> valuesList) {
+    writeHistoryData(port, pathList, dataTypeList, new ArrayList<>(), valuesList);
+  }
+
+  @Override
+  public void writeSpecialHistoryData() {
+    doWriteHistoryData(
+        Constant.readOnlyPort,
+        Constant.READ_ONLY_FLOAT_PATH_LIST,
+        new ArrayList<>(Collections.singletonList(DataType.FLOAT)),
+        Arrays.asList("key1", "key2"),
+        Constant.READ_ONLY_FLOAT_VALUES_LIST);
+  }
+
+  @Override
+  public void clearHistoryDataForGivenPort(int port) {
+    try (Driver driver = createBoltDriver(port);
+        Session session = driver.session()) {
+      Neo4jClientUtils.clearDatabase(session);
+      LOGGER.info("clear data on " + LOCAL_IP + ":{} success!", port);
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Driver createBoltDriver(int port) {
+    return createBoltDriver(LOCAL_IP, port, USER, PASSWORD);
+  }
+
+  private Driver createBoltDriver(String ip, int port, String user, String password) {
+    return GraphDatabase.driver(
+        "bolt://" + ip + ":" + port,
+        AuthTokens.basic(user, password),
+        Config.builder()
+            .withMaxConnectionPoolSize(5)
+            .withConnectionTimeout(10000, TimeUnit.MILLISECONDS)
+            .withConnectionLivenessCheckTimeout(300, java.util.concurrent.TimeUnit.SECONDS)
+            .build());
+  }
+}
