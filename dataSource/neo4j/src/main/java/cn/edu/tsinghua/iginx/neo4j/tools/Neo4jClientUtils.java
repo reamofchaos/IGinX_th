45e633443e57e881ea58324580d5a40f1ef2660b
chaos <958273999@qq.com>
2025-04-08 00:49:48 +0800

    1

--- dataSource/neo4j/src/main/java/cn/edu/tsinghua/iginx/neo4j/tools/Neo4jClientUtils.java
+++ dataSource/neo4j/src/main/java/cn/edu/tsinghua/iginx/neo4j/tools/Neo4jClientUtils.java
@@ -345,8 +345,8 @@ public class Neo4jClientUtils {
 
   public static boolean clearDatabase(Session session) {
     clearConstraint(session);
-    session.run("MATCH ()-[r]->() CALL { WITH r DELETE r } IN TRANSACTIONS OF 1000 ROWS;").consume();
-    String query = "CALL { MATCH (n) DELETE n } IN TRANSACTIONS OF 1000 ROWS;";
+//    session.run("MATCH ()-[r]->() CALL { WITH r DELETE r } IN TRANSACTIONS OF 1000 ROWS;").consume();
+    String query = "CALL apoc.periodic.iterate(\"MATCH (n) DETACH DELETE n\",  {batchSize: 5000})";
     LOGGER.info("query: {}", query);
     session.run(query).consume();
     return true;
